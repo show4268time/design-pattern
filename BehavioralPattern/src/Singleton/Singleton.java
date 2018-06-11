77d920cbb5c4edfe455e98c0323d198cd500871a
yaoweihao <yaoweihao@jd.com>
2018-06-11 20:49:53 +0800

    单例模式

---
+++ BehavioralPattern/src/Singleton/Singleton.java
@@ -0,0 +1,13 @@
+package Singleton;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @description:
+ * @author: yaoweihao
+ * @date: 2018/6/11
+ * @time: 20:49
+ * @modified by:
+ */
+public class Singleton {
+}
