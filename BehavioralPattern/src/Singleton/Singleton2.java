3beec302ae9f68f5c9dbe78d17e9dab1b87df186
yaoweihao <yaoweihao@jd.com>
2018-06-14 20:46:47 +0800

    静态内部类方法

---
+++ BehavioralPattern/src/Singleton/Singleton2.java
@@ -0,0 +1,24 @@
+package Singleton;
+
+/**
+ * Created with IntelliJ IDEA
+ * 这种方式同样利用了 classloder 机制来保证初始化 instance 时只有一个线程，
+ * 这种方式是 Singleton 类被装载了，instance 不一定被初始化。因为 SingletonHolder 类没有被主动使用，只有通过显式调用 getInstance 方法时，
+ * 才会显式装载 SingletonHolder 类，从而实例化 instance。
+ * @description: 静态内部类方法
+ * @author: yaoweihao
+ * @date: 2018/6/14
+ * @time: 20:40
+ * @modified by:
+ */
+public class Singleton2 {
+    private Singleton2(){}
+
+    private static class SingletonHolder{
+        private static Singleton2 instance = new Singleton2();
+    }
+
+    public static Singleton2 getInstance() {
+        return SingletonHolder.instance;
+    }
+}
