f8c746b3eeebd036fd2a8e9d3ba9d2a7f982259c
yaoweihao <yaoweihao@jd.com>
2018-07-08 17:13:14 +0800

    变成maven项目

--- src/main/java/Singleton/SingletonTest.java
+++ src/main/java/Singleton/SingletonTest.java
@@ -15,12 +15,12 @@ import java.lang.reflect.InvocationTargetException;
 public class SingletonTest {
     public static void main(String[] str) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         //获得构造器
-        Constructor<Singleton2> con = Singleton2.class.getDeclaredConstructor();
+        Constructor<StaticInnerSingleton> con = StaticInnerSingleton.class.getDeclaredConstructor();
         //设置为可访问
         con.setAccessible(true);
         //构造两个不同的对象
-        Singleton2 singleton1 =  con.newInstance();
-        Singleton2 singleton2 =  con.newInstance();
+        StaticInnerSingleton singleton1 =  con.newInstance();
+        StaticInnerSingleton singleton2 =  con.newInstance();
         //验证是否是不同对象
         System.out.println(singleton1.equals(singleton2));
     }
